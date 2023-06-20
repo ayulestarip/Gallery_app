b022e4e218d8905d88ad1a818008ecd654b8a96e
ayulestarip <ayulestx@gmail.com>
2023-06-20 21:52:59 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Ayu Lestari Prayudin" // TODO: replace this
+const studentId = "FE4431268" // TODO: replace this
+
+export { url, studentName, studentId }
